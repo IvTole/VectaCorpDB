40964496f5b8a6b781cd53c11be470c7e2ccd6ba
IvTole <toledano.ivan16@gmail.com>
2026-05-12 20:46:19 -0600

    update add section, add delete and update section

--- src/app_visualize.py
+++ src/app_visualize.py
@@ -3,13 +3,36 @@ import pandas as pd
 import db
 from objects import Employee
 
+# Functions
+def add_employee(name, email, role, username, password):
+    employee = Employee(name=name,
+                            email=email,
+                            roleid=db.get_roleid_from_role(str(role)),
+                             username=username,
+                             password=password)
+    db.add_employee(employee)
+
+def update_employee(employeeid, name, email, role, username, password):
+    employee = Employee(employeeid = employeeid,
+                            name=name,
+                            email=email,
+                            roleid=roleid,
+                            username=username,
+                            password=password)
+    db.update_employee(employee)
+
+    st.write("Uploading user data to database.")
+    
+    return None
+
+
 st.title("Employee Vecta Corp Menu")
 
 # Conexión a la base de datos sqlite
 db.connect()
 
 # Crear una lista de opciones
-opciones = ["View", "Add", "Delete"]
+opciones = ["View", "Add", "Delete", "Update"]
 
 # Crear el menú desplegable
 selection = st.selectbox("Selecciona una opción:", opciones)
@@ -19,7 +42,7 @@ if selection == "View":
     employees = db.get_employees()
 
     columns = ["employeeid", "name", "username",
-               "password", "email", "roleid"]
+               "password", "email", "role"]
     df = pd.DataFrame(columns=columns)
 
     for employee in employees:
@@ -29,7 +52,7 @@ if selection == "View":
             'username':[employee.username],
             'password':[employee.password],
             'email':[employee.email],
-            'roleid':[db.get_role_from_roleid(employee.roleid)]
+            'role':[db.get_role_from_roleid(employee.roleid)]
         }
         new_df = pd.DataFrame(new_data)
         df = pd.concat([df, new_df])
@@ -51,13 +74,79 @@ if selection== "Add":
             username = st.text_input(label="UserName")
             password = st.text_input(label="Password")
 
-        st.form_submit_button(label="Submit", type='primary')
+        submitted = st.form_submit_button(label="Submit", type='primary')
 
-        employee = Employee(name=name,
-                            email=email,
-                            roleid=db.get_roleid_from_role(str(role)),
-                             username=username,
-                             password=password)
+        if submitted:
+            add_employee(name, email,role, username, password)
+
+if selection=="Delete":
+
+    with st.form("Delete Employee Form"):
+
+        employeeid = st.text_input(label="Put the employee id:")
+
+        delete_button = st.form_submit_button(label="Show description", type="primary")    
+
+        if delete_button:
+            employee = db.get_employee(employeeid)
+
+            st.write(f"Employee Name: {employee.name}")
+            st.write(f"Username: {employee.username}")
+            st.write(f"Role: {db.get_role_from_roleid(employee.roleid)}")
+
+            st.subheader("Are you sure?")
         
-        db.add_employee(employee)
-        st.write("Uploading user data to database.")
+        ok_button = st.form_submit_button(label="Delete", type="primary")
+
+        if ok_button:
+            db.delete_employee(employeeid)
+            st.write(f"Employee has been removed.")
+
+if selection=="Update":
+
+    with st.form("Submit Form"):
+
+        employeeid = st.text_input(label="Put the employee id:")
+
+        show_button = st.form_submit_button(label="Show description", type="primary") 
+
+        name = ""
+        email = ""
+        roleid = 0
+        username = ""
+        password = ""
+
+        if show_button:
+            employee = db.get_employee(employeeid)
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                name = st.text_input(label="Name", value=employee.name)
+                email = st.text_input(label="Email", value=employee.email)
+                roleid = st.text_input(label="RoleID", value=employee.roleid)
+
+            with col2:
+                username = st.text_input(label="UserName", value=employee.username)
+                password = st.text_input(label="Password", value=employee.password)
+
+        update = st.form_submit_button(label="Update", type='primary')
+
+        if update:
+            st.write(f"Updating info ...")
+            update_employee(employeeid, name, email, roleid, username, password)
+        
+
+           
+
+        
+    
+    #@st.dialog("Are you sure")
+    #def Box_Delete(employeeid):
+    #    pass
+
+    #if Box_Delete not in st.session_state:
+
+        
+        
+        
